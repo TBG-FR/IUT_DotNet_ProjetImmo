7b910d22c134d02af45bd3d7376fbc4c6938ecbd
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2018-03-02 17:41:02 +0100

    Initial Commit - Base Project (Visual SLN & Proj)

---
+++ ProjetImmo.Core/Models/Estate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ProjetImmo.Core.Models
+{
+    public class Estate : ViewModels.BaseNotifyPropertyChanged
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID
+        {
+            get { return GetProperty<int>(); }
+            set { SetProperty(value); }
+        }
+
+    }
+}
