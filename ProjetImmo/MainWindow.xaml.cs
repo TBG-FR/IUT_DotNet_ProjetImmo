f1ea58f335519dac3c302ffd09eadb51720a8308
rattlesneak <36234638+rattlesneak@users.noreply.github.com>
2018-03-12 15:40:49 +0100

    IHM - Initial Commit & Design (Search bar)

--- ProjetImmo/MainWindow.xaml.cs
+++ ProjetImmo/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ProjetImmo
         {
             InitializeComponent();
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
