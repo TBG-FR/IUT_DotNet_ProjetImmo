eab6197a6f46010ab64339d09a9fc4e388efd625
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2018-03-15 10:16:51 +0100

    Update ManageEstatesViewModel.cs

--- ProjetImmo.Core/ViewModels/ManageEstatesViewModel.cs
+++ ProjetImmo.Core/ViewModels/ManageEstatesViewModel.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ProjetImmo.Core.ViewModels.ManageEstatesPage
+namespace ProjetImmo.Core.ViewModels
 {
     public class ManageEstatesViewModel : BaseNotifyPropertyChanged
     {
