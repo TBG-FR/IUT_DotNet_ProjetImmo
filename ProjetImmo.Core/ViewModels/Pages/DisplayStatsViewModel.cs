5cafde8bdf4e86e2045c842d605ac586073385b7
Tom-Brian Garcia <tom-brian.garcia@etu.univ-lyon1.fr>
2018-03-21 12:44:53 +0100

    [WIP] DisplayStatsPage

--- ProjetImmo.Core/ViewModels/Pages/DisplayStatsViewModel.cs
+++ ProjetImmo.Core/ViewModels/Pages/DisplayStatsViewModel.cs
@@ -7,5 +7,12 @@ namespace ProjetImmo.Core.ViewModels
 {
     public class DisplayStatsViewModel : BaseNotifyPropertyChanged
     {
+
+        //CurrentRentals
+        //CountToRent
+
+        //CurrentSales
+        //CountToSale
+
     }
 }
