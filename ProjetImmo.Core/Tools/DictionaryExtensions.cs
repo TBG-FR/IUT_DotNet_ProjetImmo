ddbb3fa39786d98f2c91b3ff1bc915aa9ab83bfb
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2018-03-29 13:12:44 +0200

    [WIP] LiveCharts integration
    Added many code, functions, classes and modified DislayStats design in order to add charts
    
    ERROR : assembly error WPF into Core

---
+++ ProjetImmo.Core/Tools/DictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjetImmo.Core.Tools
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<K, V> ResetValues<K, V>(this Dictionary<K, V> dic)
+        {
+            dic.Keys.ToList().ForEach(x => dic[x] = default(V));
+            return dic;
+        }
+
+        public static Dictionary<K, V> ResetValuesWithNewDictionary<K, V>(this Dictionary<K, V> dic)
+        {
+            return dic.ToDictionary(x => x.Key, x => default(V), dic.Comparer);
+        }
+
+    }
+}
